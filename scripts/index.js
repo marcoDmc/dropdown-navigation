1df8035166da908e3ae1a27b7d0545f00d9bd1ea
marcos <marcodamasceno07@gmail.com>
2022-05-30 02:38:21 -0300

    :tada: begin aproject

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+const menu = document.querySelector(".header__menu-hamburguer");
+const headerBack = document.querySelector(".header__page-background");
+const headerMobile = document.querySelector(".header__page-menu-mobile");
+const menuOpen = document.querySelector(".header__page-menu");
+const headerPageCompany = document.querySelector(".header__page-list-company");
+const headerPageFeatures = document.querySelector(
+  ".header__page-list-features"
+);
+const company = document.querySelector(".header__company");
+const features = document.querySelector(".header__features");
+const arrowImgCompany = document.querySelector(".icon-company");
+const arrowImgFeatures = document.querySelector(".icon-features");
+
+
+
+function handleOpenListCompany() {
+
+
+  headerPageCompany.classList.toggle("header__page-list-company-open");
+
+  if (
+    headerPageCompany.classList ==
+    "header__page-list-company header__page-list-company-open"
+  ) {
+    arrowImgCompany.setAttribute("src", "../assets/icon-arrow-up.svg");
+  } else {
+    arrowImgCompany.setAttribute("src", "../assets/icon-arrow-down.svg");
+  }
+}
+
+function handleOpenListFeatures() {
+  headerPageFeatures.classList.toggle("header__page-list-features-open");
+
+  if (
+    headerPageFeatures.classList ==
+    "header__page-list-features header__page-list-features-open"
+  ) {
+    arrowImgFeatures.setAttribute("src", "../assets/icon-arrow-up.svg");
+  } else {
+    arrowImgFeatures.setAttribute("src", "../assets/icon-arrow-down.svg");
+  }
+}
+
+function handleOpenPageMenu() {
+  menuOpen.classList.toggle("header__page-menu-open");
+}
+function handleOpenMenuHamburguer() {
+  headerBack.classList.toggle("header__page-background-on");
+  headerMobile.classList.toggle("header__page-menu-mobile-on");
+}
+
+function handleClickMenuOpen() {
+  menu.classList.toggle("header__menu-hamburguer-open");
+  handleOpenMenuHamburguer();
+  handleOpenPageMenu();
+}
+
+menu.addEventListener("click", handleClickMenuOpen);
+company.addEventListener("click", handleOpenListCompany);
+features.addEventListener("click", handleOpenListFeatures);
