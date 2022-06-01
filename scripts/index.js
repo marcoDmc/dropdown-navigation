ce2e88256eda9c89cbd920c053ea467467c62832
mdev <marcodamasceno07@gmail.com>
2022-06-01 06:12:42 -0300

    :construction: finalized project

--- scripts/index.js
+++ scripts/index.js
@@ -11,48 +11,40 @@ const features = document.querySelector(".header__features");
 const arrowImgCompany = document.querySelector(".icon-company");
 const arrowImgFeatures = document.querySelector(".icon-features");
 
-
-
 function handleOpenListCompany() {
-
-
   headerPageCompany.classList.toggle("header__page-list-company-open");
 
-  if (
-    headerPageCompany.classList ==
-    "header__page-list-company header__page-list-company-open"
-  ) {
-    arrowImgCompany.setAttribute("src", "../assets/icon-arrow-up.svg");
-  } else {
-    arrowImgCompany.setAttribute("src", "../assets/icon-arrow-down.svg");
-  }
+  headerPageCompany.classList ==
+  "header__page-list-company header__page-list-company-open"
+    ? arrowImgCompany.setAttribute("src", "../assets/icon-arrow-up.svg")
+    : arrowImgCompany.setAttribute("src", "../assets/icon-arrow-down.svg");
 }
 
 function handleOpenListFeatures() {
   headerPageFeatures.classList.toggle("header__page-list-features-open");
 
-  if (
-    headerPageFeatures.classList ==
-    "header__page-list-features header__page-list-features-open"
-  ) {
-    arrowImgFeatures.setAttribute("src", "../assets/icon-arrow-up.svg");
-  } else {
-    arrowImgFeatures.setAttribute("src", "../assets/icon-arrow-down.svg");
-  }
+  headerPageFeatures.classList ==
+  "header__page-list-features header__page-list-features-open"
+    ? arrowImgFeatures.setAttribute("src", "../assets/icon-arrow-up.svg")
+    : arrowImgFeatures.setAttribute("src", "../assets/icon-arrow-down.svg");
 }
 
 function handleOpenPageMenu() {
   menuOpen.classList.toggle("header__page-menu-open");
 }
 function handleOpenMenuHamburguer() {
-  headerBack.classList.toggle("header__page-background-on");
-  headerMobile.classList.toggle("header__page-menu-mobile-on");
+  headerBack.classList.toggle("header__page-background-open");
+  handleOpenPageMenu();
 }
 
 function handleClickMenuOpen() {
   menu.classList.toggle("header__menu-hamburguer-open");
+
+  menu.classList == "header__menu-hamburguer header__menu-hamburguer-open"
+    ? menu.setAttribute("src", "../assets/icon-close-menu.svg")
+    : menu.setAttribute("src", "../assets/icon-menu.svg");
+
   handleOpenMenuHamburguer();
-  handleOpenPageMenu();
 }
 
 menu.addEventListener("click", handleClickMenuOpen);
